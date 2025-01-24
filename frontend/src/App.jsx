1c60aed36db04a1728f93a6c4c3eacb32b2f15b9
srrohit110 <rohit.talaviya@swiftrut.com>
2025-01-24 22:51:38 +0530

    "Deleted frontend/index.jsx file"

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold bg-blue-300 ">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
